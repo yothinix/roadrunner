6396e5d7ad7348c983e6fe53971add6ad9b26c1d
Yothin M <m@yothinix.com>
2019-05-12 13:14:51 +0700

    Use echo to control print :rocket:

--- tasks.py
+++ tasks.py
@@ -1,31 +1,37 @@
 from invoke import task
 
 
+def echo(command):
+    print("--------------")
+    print(command)
+    print("--------------")
+
+
 @task
 def black(c):
     command = "python -m black --target-version py36 --line-length 99 ."
-    print(command)
+    echo(command)
     c.run(command)
 
 
 @task
 def isort(c):
     command = "python -m isort --apply --line-width 99"
-    print(command)
+    echo(command)
     c.run(command)
 
 
 @task
 def flake8(c):
     command = "python -m flake8 --max-line-length=99"
-    print(command)
+    echo(command)
     c.run(command)
 
 
 @task
 def pytest(c):
     command = "pytest"
-    print(command)
+    echo(command)
     c.run(command)
 
 
