763192c6d0a2ecaaafc8e3e6b355fab3debed2ff
Yothin M <m@yothinix.com>
2019-05-25 14:53:53 +0700

    Add spinner in task :rocket:

--- tasks.py
+++ tasks.py
@@ -1,40 +1,45 @@
+from halo import Halo
 from invoke import task
 
 
 def echo(command):
-    print("--------------")
-    print(command)
-    print("--------------")
+    spinner = Halo(text=command, text_color="green", spinner="dots")
+    spinner.start()
+    return spinner
 
 
 @task
 def black(c):
     command = "python -m black --target-version py36 --line-length 99 ."
-    echo(command)
+    spinner = echo(command)
     c.run(command)
+    spinner.stop()
 
 
 @task
 def isort(c):
     command = "python -m isort --apply --line-width 99"
-    echo(command)
+    spinner = echo(command)
     c.run(command)
+    spinner.stop()
 
 
 @task
 def flake8(c):
     command = "python -m flake8 --max-line-length=99"
-    echo(command)
+    spinner = echo(command)
     c.run(command)
+    spinner.stop()
 
 
 @task
 def pytest(c):
-    command = "pytest"
-    echo(command)
+    command = "pytest --color=yes"
+    spinner = echo(command)
     c.run(command)
+    spinner.stop()
 
 
 @task(pre=[black, isort, flake8, pytest])
 def build(c):
-    print("---finishing build---")
+    pass
